e1760db8cca4446a07677d25dec40a6f026f55fd
SivaSundarirk <sivasundarirk@gmail.com>
2025-09-13 13:17:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    /**
+     * print my personal information like name,address,hobby
+     * and give the constant value for my age
+     * @param args
+     */
+    public static final int age=18;
+    public static void main(String[] args) {
+        String name = "Siva Sundari R K";
+        String address = "Thoothukudi";
+        String Hobby = "Dancing and Acting";
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(Hobby);
+        System.out.println(age);
+
+    }
+}
