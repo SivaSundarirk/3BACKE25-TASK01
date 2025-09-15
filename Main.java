1f3399ddd47cc47818262f30a0371088d2670688
SivaSundarirk <sivasundarirk@gmail.com>
2025-09-15 23:38:22 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,25 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main
-{
+public class Main {
+    public static void main(String[] args)
     /**
-     * print my personal information like name,address,hobby
-     * and give the constant value for my age
+     * Hotel Bill Calculator;
+     * Adds item,prices,applies GST and prints the total bill
      * @param args
      */
-    public static final int age=18;
-    public static void main(String[] args) {
-        String name = "Siva Sundari R K";
-        String address = "Thoothukudi";
-        String Hobby = "Dancing and Acting";
-        System.out.println(name);
-        System.out.println(address);
-        System.out.println(Hobby);
-        System.out.println(age);
+    {
+        double whitepasta=200;
+    double Mangojucie=120;
+    double chocolateicecream=89;
+    double price=whitepasta+Mangojucie+chocolateicecream;
+    double GST=price*0.12;
+    double totalprice=price+GST;
+    System.out.println("Whitepasta:" +whitepasta);
+    System.out.println("Mangojuice:" +Mangojucie);
+    System.out.println("Chocolateicecream:" +chocolateicecream);
+    System.out.println("Price=" +price);
+    System.out.println("GST=" +GST);
+    System.out.println("Totalprice=" +totalprice);
 
     }
 }
